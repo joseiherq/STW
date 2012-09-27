7f3602071dbe8edfe60de14faf2607b9e84c9ebc
usuario <joseiher@gmail.com>
2012-09-27 16:45:20 +0100

    practica29

--- Practica1/Rock-Paper-Scissors.rb
+++ Practica1/Rock-Paper-Scissors.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'erb'
 
+enable :sessions
+
 # before we process a route we'll set the response as plain text
 # and set up an array of viable moves that a player (and the
 # computer) can perform
@@ -10,9 +12,21 @@ before do
 end
 
 get '/' do
+  session['wins'] = 0
+  session['ties'] = 0
+  session['losses'] = 0
+  @wins = session['wins']
+  @ties = session['ties']
+  @losses = session['losses']
   erb :form
 end
 
+get '/home' do
+  @wins = session['wins']
+  @ties = session['ties']
+  @losses = session['losses']
+  erb :form
+end
 
 get '/throw/:type' do
   # the params hash stores querystring and form data
@@ -25,12 +39,15 @@ get '/throw/:type' do
   if @player_throw == @computer_throw 
     @answer = "There is a tie"
 	 @img= "tie"
+	 session['ties'] += 1
   elsif @player_throw == @defeat[@computer_throw]
     @answer = "Sorry. #{@computer_throw} beats #{@player_throw}"
 	 @img= "sorry"
+	 session['losses'] += 1
   else
     @answer = "Well done. #{@player_throw} beats #{@computer_throw}"
 	 @img= "congrats"
+	 session['wins'] += 1
   end
   erb :result
 end
@@ -40,6 +57,6 @@ post '/throw' do
 	if @params == "rock" || @params == "paper" || @params == "scissors"
 		redirect "/throw/#{@params}"
 	end
-	redirect "/"
+	redirect "/home"
 end
 
